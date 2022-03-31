6fc5a4c09b400fb2611f36097ce71f3fab013385
Samirza0912 <samirza@code.edu.az>
2022-04-01 03:35:41 +0400

    feat:i add task about generic and collections

---
+++ Generic_Collection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
